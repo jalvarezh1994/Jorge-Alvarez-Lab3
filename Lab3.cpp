b8b0f188b940d3f809b7fcd264324f0ea2c22f25
Jorge <j.alvarezh1994@unitec.edu>
2017-05-05 13:20:47 -0600

    Comienza el proyecto

--- Lab3.cpp
+++ Lab3.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+
 int main(){
-
-
+	int opcion;
+	do{
+		cout<<"Menu"<<endl;
+		cout<<"1. Ejercicio 1"<<endl;
+		cout<<"2. Ejercicio 2"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Ingrese la opcion: ";
+		cin<<opcion;
+		switch(opcion){
+			case 1:{
+				break;
+			}
+			case 2:{
+				break;
+			}
+			case 3:{
+				break;
+			}
+		}
+	}while(opcion!=3);
 	return 0;
 }
