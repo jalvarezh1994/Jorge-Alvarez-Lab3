0047a9202b0e624301d613dea5b2266acf2cf9a2
Jorge <j.alvarezh1994@unitec.edu>
2017-05-05 14:19:41 -0600

    Comienza el proyecto

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string decimalBinario(int);
 
 int main(){
 	int opcion;
@@ -11,9 +13,13 @@ int main(){
 		cout<<"2. Ejercicio 2"<<endl;
 		cout<<"3. Salir"<<endl;
 		cout<<"Ingrese la opcion: ";
-		cin<<opcion;
+		cin>>opcion;
 		switch(opcion){
 			case 1:{
+				int numero;
+				cout<<"Ingrese el número: ";
+				cin>>numero;
+				cout<<decimalBinario(numero)<<endl;
 				break;
 			}
 			case 2:{
@@ -25,4 +31,19 @@ int main(){
 		}
 	}while(opcion!=3);
 	return 0;
+}
+
+string decimalBinario(int decimal){
+	int cifras=0;
+	int resto;
+	string binario1="";
+	string binario2="";
+	do{	
+		resto=decimal%2;
+		binario1=binario1+10*cifras+resto;
+		decimal=decimal/2;
+		cifras++;	
+	}while(decimal/2>0);
+	binario2=binario1;
+	return binario2;
 }
